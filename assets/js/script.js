4175bb4a0f82b59680f1458fbf4c71cedc77a548
Caitlin O'Reilly <coreilly29@gmail.com>
2022-08-26 20:47:10 -0400

    started to convert lists in sortables

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,5 +179,40 @@ $(".list-group").on("blur", "input[type='text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
+// tasks can now be dragged within the same column and across other columns.
+// use jQuery selector to find all list-group elements w/ class list-group. Then use jQuery slector to find all list-group selements, then call a new jQuery UI method on them.
+// jQuery UI method sortable() turned every element w/ the class list-group into a sortable list
+// connectWith property linked these sortable lists w/ any other lists that have the same class
+$(".card .list-group").sortable({
+  connectWith: ".card .list-group",
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        console.log(text, date);
+      });
+  },
+});
+
 // load tasks for the first time
 loadTasks();
